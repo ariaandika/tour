70a63cc5377e9f7ae5e218c34fb6a05cfbd2411e
ariaandika <andikaakun17@gmail.com>
2025-06-03 23:06:39 +0700

    add block tests

---
+++ tour/tests/block.rs
@@ -0,0 +1,28 @@
+use tour::Template;
+
+#[test]
+fn blocked() {
+    #[derive(Template)]
+    #[template(root = "tour/tests/block/block.html")]
+    struct Root;
+
+    let p = Root.render().unwrap();
+    assert_eq!(
+        &p[..],
+        "<div><p>Willie</p></div>
+<div><p>Jane</p></div>
+<p>Willie</p>
+<p>Jane</p>
+");
+}
+
+#[test]
+fn block() {
+    #[derive(Template)]
+    #[template(root = "tour/tests/block/block.html", block = Willie)]
+    struct Block;
+
+    let p = Block.render().unwrap();
+    assert_eq!(&p[..], "<p>Willie</p>");
+}
+
