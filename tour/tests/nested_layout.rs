c836b8fbac11a449b544078f7925b6b95d3336d2
ariaandika <andikaakun17@gmail.com>
2025-06-03 21:19:10 +0700

    isolate nested layout test

---
+++ tour/tests/nested_layout.rs
@@ -0,0 +1,21 @@
+use tour::Template;
+
+#[test]
+fn nested_layout() {
+    #[derive(Template)]
+    #[template(root = "tour/tests/basic.html")]
+    struct NestedLayout {
+        name: String,
+    }
+
+    let layout = NestedLayout { name: "barred".into() }.render_layout().unwrap();
+    assert_eq!(
+        &layout[..],
+        r#"<section id="layout1"><section id="layout2"><section id="layout3">Token barred once
+</section>
+</section>
+</section>
+"#
+    );
+}
+
