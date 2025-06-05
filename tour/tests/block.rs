83bcccbef22e59ef6b547dae5e57475fa6dd56da
ariaandika <andikaakun17@gmail.com>
2025-06-05 16:16:24 +0700

    add test for block operation

--- tour/tests/block.rs
+++ tour/tests/block.rs
@@ -13,16 +13,27 @@ fn blocked() {
 <div><p>Jane</p></div>
 <p>Willie</p>
 
+
 ");
 }
 
 #[test]
-fn block() {
+fn block_comptime() {
     #[derive(Template)]
     #[template(root = "tour/tests/block/block.html", block = Willie)]
     struct Block;
 
     let p = Block.render().unwrap();
     assert_eq!(&p[..], "<p>Willie</p>");
+}
+
+#[test]
+fn block_runtime() {
+    #[derive(Template)]
+    #[template(root = "tour/tests/block/block.html")]
+    struct Block;
+
+    let p = Block.render_block("Body").unwrap();
+    assert_eq!(&p[..], "<h1>Title</h1>");
 }
 
